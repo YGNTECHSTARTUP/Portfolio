e6b8a42ff909fea94f322b70884c3f739fd58261
YGN <ygntechstartup@gmail.com>
2024-07-09 15:43:53 +0530

    techstack

--- app/MyTechStack/page.tsx
+++ app/MyTechStack/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import GridBackgroundDemo from '../components/ui/dotbackround'
+import Image from 'next/image'
+
 
 const page = () => {
   return (
 <GridBackgroundDemo>
-    h
+<div className=' font-extrabold items-center mx-auto text-center underline text-black dark:text-white '>
+        My Tech Stack
+        <Image src={'/techstack.png'} alt='tech_stack' width={1040} height={1040}/>
+        </div>
+<div>
+
+</div>
 </GridBackgroundDemo>
 )
 }
